28d93ea3d1170b6dced84a9a6e0a06bb6eb2f4e7
Alberto Minoru <alberto.minoru@gmail.com>
2021-07-19 17:13:55 -0300

    Login e Logout

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,6 @@ MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'registros_list'
+LOGOUT_REDIRECT_URL = 'home'
