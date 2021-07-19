1c4f7b21a9ba2fbb8504429e2846bf56a20f7bb0
Alberto Minoru <alberto.minoru@gmail.com>
2021-07-19 13:18:16 -0300

    Funcionalidades para atualizar e deletar registros

--- registros/urls.py
+++ registros/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PressaoListView, PressaoDetailView, PressaoCreateView
+from .views import PressaoListView, PressaoDetailView, PressaoCreateView, PressaoUpdateView, PressaoDeleteView
 
 urlpatterns = [
     path('', PressaoListView.as_view(), name='registros_list'),
     path('<int:pk>/', PressaoDetailView.as_view(), name='registro_detail'),
     path('new/', PressaoCreateView.as_view(), name='registro_new'),
+    path('<int:pk>/edit/', PressaoUpdateView.as_view(), name='registro_edit'),
+    path('<int:pk>/delete/', PressaoDeleteView.as_view(), name='registro_delete'),
 ]
--- registros/views.py
+++ registros/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Pressao
 
 
@@ -17,3 +18,15 @@ class PressaoCreateView(CreateView):
     model = Pressao
     template_name = 'registros/registro_new.html'
     fields = ['pessoa', 'sis', 'dia', 'pul']
+
+
+class PressaoUpdateView(UpdateView):
+    model = Pressao
+    template_name = 'registros/registro_edit.html'
+    fields = ['sis', 'dia', 'pul']
+
+
+class PressaoDeleteView(DeleteView):
+    model = Pressao
+    template_name = 'registros/registro_delete.html'
+    success_url = reverse_lazy('registros_list')
