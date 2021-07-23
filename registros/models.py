571468ebb0411bcded6cb2b6a011ea5485d40eef
Alberto Minoru <alberto.minoru@gmail.com>
2021-07-23 18:50:33 -0300

    Modificações no model Pressao, agora o pulso não é obrigatório

--- registros/models.py
+++ registros/models.py
@@ -6,11 +6,15 @@ from django.urls import reverse
 
 class Pressao(models.Model):
     pessoa = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
-    data = models.DateTimeField()
-    sis = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(200)])
-    dia = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(120)])
-    pul = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(150)])
+    data = models.DateTimeField('Data')
+    sis = models.PositiveIntegerField('Sistólica', validators=[MinValueValidator(1), MaxValueValidator(200)])
+    dia = models.PositiveIntegerField('Diastólica', validators=[MinValueValidator(1), MaxValueValidator(120)])
+    pul = models.PositiveIntegerField('Pulso', validators=[MinValueValidator(1), MaxValueValidator(150)], blank=True, null=True)
     modificado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Pressão'
+        verbose_name_plural = 'Pressões'
 
     def __str__(self):
         return f"{self.pessoa} - {self.data.strftime('%d/%m/%Y %H:%M')}"
